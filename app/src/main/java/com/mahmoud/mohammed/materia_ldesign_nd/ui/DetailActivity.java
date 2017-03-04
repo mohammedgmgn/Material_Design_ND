f9a9dc705ddbb62d865ed2fe9a7b517266f0442b
mohammed mahmoud <mohammed mahmoud>
2017-03-04 23:42:31 +0100

    feat: add custom font

--- app/src/main/java/com/mahmoud/mohammed/materia_ldesign_nd/ui/DetailActivity.java
+++ app/src/main/java/com/mahmoud/mohammed/materia_ldesign_nd/ui/DetailActivity.java
@@ -2,6 +2,7 @@ package com.mahmoud.mohammed.materia_ldesign_nd.ui;
 
 import android.content.res.Resources;
 import android.content.res.TypedArray;
+import android.graphics.Typeface;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -18,7 +19,10 @@ public class DetailActivity extends AppCompatActivity {
 
     CollapsingToolbarLayout collapsingToolbar;
     ImageView imageView;
+    TextView detail;
     TextView description;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +33,8 @@ public class DetailActivity extends AppCompatActivity {
          imageView=(ImageView)findViewById(R.id.image);
          collapsingToolbar = (CollapsingToolbarLayout)
                 findViewById(R.id.collapsing_toolbar);
-         description=(TextView)findViewById(R.id.place_detail);
+         detail=(TextView)findViewById(R.id.place_detail);
+        description=(TextView)findViewById(R.id.descriptionid);
         collapsingToolbar.setTitle(getString(R.string.item_title));
         getData();
 
@@ -41,9 +46,12 @@ public class DetailActivity extends AppCompatActivity {
         int postion=getIntent().getIntExtra(POSTION,0);
         Resources resources = getResources();
         //TypedArray mtitlesArray = resources.obtainTypedArray(R.array.places);
+        Typeface customfont=Typeface.createFromAsset(getAssets(),"Raleway-ExtraBold.ttf");
+        Typeface customfont2=Typeface.createFromAsset(getAssets(),"Fonty.ttf");
 
-        description.setText(desc);
-
+        description.setTypeface(customfont);
+        detail.setTypeface(customfont2);
+        detail.setText(desc);
         TypedArray mPlacePicturesArray = resources.obtainTypedArray(R.array.places_picture);
         imageView.setImageDrawable(mPlacePicturesArray.getDrawable(postion));
         collapsingToolbar.setTitle(title);
